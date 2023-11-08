72001cd644e87f34ad2cb80a5554ec708e28c864
Castro-1 <jecastrog@eafit.edu.co>
2023-11-08 12:16:39 -0500

    styled select

--- src/components/inputs/Selelct.jsx
+++ src/components/inputs/Selelct.jsx
@@ -1,7 +1,7 @@
 export default function Select({id, children, ...props}){
     return <div>
     <label htmlFor={id} className="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">{id}</label>
-    <select id={id} className="border text-sm rounded-lg p-2.5 w-full dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 [&>option]:bg-[#090916] [&>option]:border-gray-600 [&>option]:" {...props}>
+    <select id={id} className="border text-sm rounded-lg p-2.5 w-full bg-[#090916] text-white focus:ring-blue-500 focus:border-blue-500" {...props}>
       <option value={""}>Select an option</option>
       {children}
     </select>

63d8b311e6fb6b134a2b0fccbb083fa477a7ed13
Castro-1 <jecastrog@eafit.edu.co>
2023-11-20 18:19:50 -0500

    predict format

--- src/components/inputs/Selelct.jsx
+++ src/components/inputs/Selelct.jsx
@@ -1,9 +1,19 @@
-export default function Select({id, children, ...props}){
-    return <div>
-    <label htmlFor={id} className="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">{id}</label>
-    <select id={id} className="border text-sm rounded-lg p-2.5 w-full bg-[#090916] text-white focus:ring-blue-500 focus:border-blue-500" {...props}>
-      <option value={""}>Select an option</option>
-      {children}
-    </select>
+export default function Select({ id, children, ...props }) {
+  return (
+    <div>
+      <label
+        htmlFor={id}
+        className="block mb-2 text-sm font-semibold text-gray-900 dark:text-white"
+      >
+        {id}
+      </label>
+      <select
+        id={id}
+        className="border text-sm rounded-lg p-2.5 w-full bg-[#090916] text-white focus:ring-blue-500 focus:border-blue-500"
+        {...props}
+      >
+        {children}
+      </select>
     </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import StarsCanvas from "../components/Stars";
 export default function Home() {
   const [ligthIntensity, setLightIntensity] = useState(0.2);
   const [values, setValues] = useState({
-    status: "",
+    status: "0",
     bed: "",
     bath: "",
     acre_lot: "",
@@ -17,79 +17,151 @@ export default function Home() {
     state: "",
     zip_code: "",
     house_size: "",
-    prev_sold_date: ""
+    prev_sold_date: "",
   });
   const [result, setResult] = useState(null);
 
-  const handleValues = (ev) =>{
-    if(values[ev.target.name] === ""){
-      setLightIntensity(prev=>prev+=0.1);
+  const handleValues = (ev) => {
+    if (values[ev.target.name] === "") {
+      setLightIntensity((prev) => (prev += 0.1));
     }
-    if (ev.target.value === ""){
-      setLightIntensity(prev=>prev-=0.1);
+    if (ev.target.value === "") {
+      setLightIntensity((prev) => (prev -= 0.1));
     }
-    setValues(prev=>({
-      ...prev, [ev.target.name]: ev.target.value,
-    }))
-  }
+    setValues((prev) => ({
+      ...prev,
+      [ev.target.name]: ev.target.value,
+    }));
+  };
 
-  const handleSubmit = async () =>{
+  const handleSubmit = async () => {
     const data = {
-      status: values.status,
+      status: parseFloat(values.status),
       bed: parseFloat(values.bed),
       bath: parseFloat(values.bath),
       acre_lot: parseFloat(values.acre_lot),
-      city: values.city,
-      state: values.state,
+      city: parseFloat(values.city),
+      state: parseFloat(values.state),
       zip_code: parseFloat(values.zip_code),
       house_size: parseFloat(values.house_size),
-      prev_sold_date: values.prev_sold_date
-    }
+      prev_sold_date: parseFloat(values.prev_sold_date),
+    };
     console.log(data);
-    const response = await axios.post("http://127.0.0.1:8000", data);
-    if(response){
-      setResult(response.data)
+    const response = await axios.post(
+      "https://house-wise-api.vercel.app",
+      data
+    );
+    if (response) {
+      setResult(response.data);
     }
-  }
+  };
 
-  return <div className="grid grid-cols-3">
-    <div className="grid grid-cols-2 gap-3 border p-8 ml-10 rounded-lg">
-      <Select id={"Status"} name="status" onChange={handleValues} value={values.status}>
-        <option value={0}>For sale</option>
-        <option value={1}>Ready to build</option>
-      </Select>
-      <Input placeholder="3" id={"Bedrooms"} name="bed" onChange={handleValues} value={values.bed}/>
-      <Input placeholder="2" id={"Bathrooms"} name="bath" onChange={handleValues} value={values.bath}/>
-      <Input placeholder="0.12" id={"Land size (acres)"} name="acre_lot" onChange={handleValues} value={values.acre_lot}/>
-      <Select id={"City"} name="city" onChange={handleValues} value={values.city}>
+  return (
+    <div className="grid grid-cols-3">
+      <div className="grid grid-cols-2 gap-3 border p-8 ml-10 rounded-lg">
+        <Select
+          id={"Status"}
+          name="status"
+          onChange={handleValues}
+          value={values.status}
+        >
+          <option value={0}>For sale</option>
+          <option value={1}>Ready to build</option>
+        </Select>
+        <Input
+          placeholder="3"
+          id={"Bedrooms"}
+          name="bed"
+          onChange={handleValues}
+          value={values.bed}
+        />
+        <Input
+          placeholder="2"
+          id={"Bathrooms"}
+          name="bath"
+          onChange={handleValues}
+          value={values.bath}
+        />
+        <Input
+          placeholder="0.12"
+          id={"Land size (acres)"}
+          name="acre_lot"
+          onChange={handleValues}
+          value={values.acre_lot}
+        />
+        <Input
+          placeholder="14.0"
+          id={"City"}
+          name="city"
+          onChange={handleValues}
+          value={values.city}
+        />
+        {/* <Select id={"City"} name="city" onChange={handleValues} value={values.city}>
         <option value={0}>Miami</option>
         <option value={1}>LA</option>
-      </Select>
-      <Select id={"State"} name="state" onChange={handleValues} value={values.state}>
-        <option value={0}>
-          Florida
-        </option>
-        <option value={1}>
-          California
-        </option>
-      </Select>
-      <Input placeholder="601.0" id={"Zip code"} name="zip_code" onChange={handleValues} value={values.zip_code}/>
-      <Input placeholder="920.0" id={"House size"} name="house_size" onChange={handleValues} value={values.house_size}/>
-      <Select id={"Previously sold date"} name="prev_sold_date" onChange={handleValues} value={values.prev_sold_date}>
-        <option value={0}>
-          today
-        </option>
-        <option value={1}>
-          yesterday
-        </option>
-      </Select>
-      <div className="flex items-end w-full [&>button]:m-0 [&>button]:w-full [&>button]:font-semibold">
-        <Button onClick={handleSubmit}>Calculate</Button>
+      </Select> */}
+        <Input
+          placeholder="9.0"
+          id={"State"}
+          name="state"
+          onChange={handleValues}
+          value={values.state}
+        />
+        {/* <Select
+          id={"State"}
+          name="state"
+          onChange={handleValues}
+          value={values.state}
+        >
+          <option value={0}>Florida</option>
+          <option value={1}>California</option>
+        </Select> */}
+        <Input
+          placeholder="601.0"
+          id={"Zip code"}
+          name="zip_code"
+          onChange={handleValues}
+          value={values.zip_code}
+        />
+        <Input
+          placeholder="920.0"
+          id={"House size"}
+          name="house_size"
+          onChange={handleValues}
+          value={values.house_size}
+        />
+        <Input
+          placeholder="0.0"
+          id={"Previously sold date"}
+          name="prev_sold_date"
+          onChange={handleValues}
+          value={values.prev_sold_date}
+        />
+        {/* <Select
+          id={"Previously sold date"}
+          name="prev_sold_date"
+          onChange={handleValues}
+          value={values.prev_sold_date}
+        >
+          <option value={0}>today</option>
+          <option value={1}>yesterday</option>
+        </Select> */}
+        <div className="flex items-end w-full [&>button]:m-0 [&>button]:w-full [&>button]:font-semibold">
+          <Button onClick={handleSubmit}>Calculate</Button>
+        </div>
       </div>
+      <div className="col-span-2">
+        <RoomCanvas ligthIntensity={ligthIntensity} />
+      </div>
+      <div className="col-span-3 text-center">
+        {result && (
+          <h2 className="text-3xl">{`Prediction: ${new Intl.NumberFormat(
+            "en-Us",
+            { style: "currency", currency: "USD" }
+          ).format(result)}`}</h2>
+        )}
+      </div>
+      <StarsCanvas />
     </div>
-    <div className="col-span-2">
-      <RoomCanvas ligthIntensity={ligthIntensity}/>
-    </div>
-    <StarsCanvas />
-  </div>;
+  );
 }
